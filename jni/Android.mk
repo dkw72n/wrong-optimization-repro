067c21d3fd3a860d83a1782fc634a2f312b68e4a
lijianjun <lijianjun@kingsoft.com>
2019-12-16 19:35:32 +0800

    corrected the arm mode used in compilation

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,13 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_ARM_MODE := arm
+LOCAL_ARM_MODE := thumb
 LOCAL_MODULE := isthumb_o2
 LOCAL_SRC_FILES := isthumb.c
 LOCAL_CFLAGS := -O2
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-LOCAL_ARM_MODE := arm
+LOCAL_ARM_MODE := thumb
 LOCAL_MODULE := isthumb_o0
 LOCAL_SRC_FILES := isthumb.c
 LOCAL_CFLAGS := -O0
